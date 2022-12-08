05d89ebca4e142d1a9de8400b6cba07b2ca3a46c
andreadana40 <andreadana40@gmail.com>
2022-12-08 20:14:46 -0300

    nuevo

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 from flaskext.mysql import MySQL
+
+# Jinja2 es un motor de plantillas que nos permite crear plantillas html dinamicas
+
 
 app = Flask(__name__)
 mysql = MySQL()
@@ -12,12 +15,42 @@ mysql.init_app(app)
 
 @app.route('/')
 def index():
-    sql = "INSERT INTO `movies_db`.`movies`(`created_at`,`updated_at`,`title`,`rating`,`awards`,`release_date`,`length`,`genre_id`) VALUES (null,null,'Pelicula de terror',10.3,3,sysdate(), 120, 2);"
+    sql = "SELECT * FROM `movies_db`.`movies`" #Creo una consulta sql para obtener todas las movies
+    conn = mysql.connect()#coneccion a base de datos
+    cursor =conn.cursor()
+    cursor.execute(sql)
+    data_movies = cursor.fetchall()
+    conn.commit()
+    return render_template('movies/index.html', movies=data_movies)
+
+@app.route('/create')
+def create():
+    return render_template('movies/create.html')
+
+@app.route('/store', methods=['POST'])
+def storage():
+    _name = request.form['name']
+    _rating = request.form['rating']
+    _awards = request.form['awards']
+    _length = request.form['length']
+    _genre_id =request.form['genre_id']
+
+
+    sql = "INSERT INTO `movies_db`.`movies`(`created_at`,`updated_at`,`title`,`rating`,`awards`,`release_date`,`length`,`genre_id`) VALUES ( sysdate(), sysdate(),'" + _name +"', " + _rating + ", " + _awards + ", sysdate()," + _length +", " + _genre_id +");"
     conn = mysql.connect()
     cursor = conn.cursor()
     cursor.execute(sql)
     conn.commit()
-    return render_template('movies/index.html')
+    return 'Pelicula creada con exito'
+
+@app.route('/delete/<id>')
+def delete(id):
+    sql = "DELETE FROM `movies_db`.`movies` WHERE `id` = " + id + ";"
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    conn.commit()
+    return 'Pelicula eliminada con exito'
 
 if __name__ == '__main__':
     app.run(debug=True)  
